8888665d916aba43d96df95903211e84666752b6
Senay Yakut <senaykt@gmail.com>
2019-04-25 16:01:40 -0700

    eval(..) or with

--- scope_closures.js
+++ scope_closures.js
@@ -38,3 +38,24 @@ function foo(a){
 }
 
 foo(2);//2 4 12
+
+function foo(obj) {
+    with (obj) {
+        a = 2;
+    }
+}
+
+var o1 = {
+    a: 3
+};
+
+var o2 = {
+    b: 3
+};
+
+foo(o1);
+console.log(o1.a); // 2
+
+foo(o2);
+console.log(o2.a); // undefined
+console.log(a); // 2 -- Oops, leaked global!
