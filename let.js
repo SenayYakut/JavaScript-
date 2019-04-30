6800c354bffd445e11161112fda0bcc004cbcf14
Senay Yakut <senaykt@gmail.com>
2019-04-29 19:24:52 -0700

    let

--- let.js
+++ let.js
@@ -11,3 +11,21 @@ if(x===1){
 console.log(x);
 //2
 //1
+
+function varTest(){
+    var x = 1;
+    if(true){
+        var x =2;
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function letTest(){
+    let x = 1;
+    if(true){
+        let x=2;
+        console.log(x);
+    }
+    console.log(x);
+}
