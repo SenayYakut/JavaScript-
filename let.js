8d8d4253387efc81ed80ebe4ad9ae90867c4f5c8
Senay Yakut <senaykt@gmail.com>
2019-05-01 20:19:04 -0700

    let vs var

--- let.js
+++ let.js
@@ -29,3 +29,29 @@ function letTest(){
     }
     console.log(x);
 }
+
+function varTest(){
+    var x =1;
+    if(true){
+        var x=2;
+        console.log(x);
+    }
+    console.log(x);
+}
+varTest();
+
+function letTest(){
+    let x =1;
+    if(true){
+        let x=2;
+        console.log(x);
+    }
+    console.log(x);
+}
+letTest();
+
+var x="global";
+let y="global";
+console.log(this.x);
+console.log(this.y);
+
